53f141d944e37451a4b4c0dbcb167379971db74c
VampirN <mari.vampir@yandex.ru>
2024-06-17 22:44:33 +0200

    change

--- src/work_with_vacancies.py
+++ src/work_with_vacancies.py
@@ -7,8 +7,22 @@ class VacanciesHH:
         self.currency = currency
         self.requirements = requirements
 
+
+        '''Валидация данных'''
+        if not isinstance(self.salary, (str, int, float)):
+            self.salary = 0
+
     def __repr__(self):
         return (f"Название вакансии: {self.name}\n "
                 f"Ссылка на вакансию: {self.url}\n "
                 f"Зарплата: {self.salary} {self.currency}\n "
                 f"Требования: {self.requirements}")
+
+    def __lt__(self, other):
+        '''Сравнение вакансий по зп'''
+        return self.salary < other.salary
+
+    def __eq__(self, other):
+        '''Проарка сходства вакансий'''
+        return (self.name == other.name and self.salary == other.salary
+                and self.currency == other.currency and self.requirements == other.requirements)
